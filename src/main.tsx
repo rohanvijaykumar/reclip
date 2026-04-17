088608bb1010e6b569dbb5fd8742278a3e5851f2
rohanvijaykumar <djatom12345@gmail.com>
2026-04-16 22:17:37 -0400

    feat: implement global theme system with glassmorphic design and Geist font integration

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from "react-dom/client";
 import { ConfigProvider } from "./contexts/ConfigContext";
 import App from "./App";
+import "@fontsource-variable/geist";
 import "./styles/index.css";
 
 createRoot(document.getElementById("root")!).render(
